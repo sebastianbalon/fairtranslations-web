92b7ea1122d8e35e1ca433e928bed8e8c616aefc
balons <balon.sebastian@gmail.com>
2020-03-27 23:46:42 +0100

    Removed comment

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withCss = require('@zeit/next-css');
 
 module.exports = {
   exportTrailingSlash: false,
-  /*exportPathMap: () => ({
-    '/p/o-firme': { page: '/p/o-firme' }
-  }),*/
   target: 'serverless',
   ...withSass(withCss()),
 }
