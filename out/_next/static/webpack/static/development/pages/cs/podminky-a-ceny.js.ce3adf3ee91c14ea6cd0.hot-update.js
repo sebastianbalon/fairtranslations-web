114b72d613526bc18ec2da28758aa78f814b1f7e
balons <balon.sebastian@gmail.com>
2020-03-27 22:40:46 +0100

    Added exported pages

---
+++ out/_next/static/webpack/static/development/pages/cs/podminky-a-ceny.js.ce3adf3ee91c14ea6cd0.hot-update.js
@@ -0,0 +1,43 @@
+webpackHotUpdate("static/development/pages/cs/podminky-a-ceny.js",{
+
+/***/ "./pages/cs/podminky-a-ceny.tsx":
+/*!**************************************!*\
+  !*** ./pages/cs/podminky-a-ceny.tsx ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Layout_Layout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../components/Layout/Layout */ "./components/Layout/Layout.tsx");
+var _jsxFileName = "/Users/balons/Development/fairtranslations-web/pages/cs/podminky-a-ceny.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+var Index = function Index() {
+  return __jsx(_components_Layout_Layout__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    language: "cs",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, __jsx("h1", {
+    className: "title",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "Podm\xEDnky a ceny"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Index);
+
+/***/ })
+
+})
+//# sourceMappingURL=podminky-a-ceny.js.ce3adf3ee91c14ea6cd0.hot-update.js.map
