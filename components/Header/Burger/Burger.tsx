1821bdf089b06cb3186474e50a5a4a4e0003756b
balons <balon.sebastian@gmail.com>
2020-03-28 00:48:41 +0100

    Added navbar Burger component

---
+++ components/Header/Burger/Burger.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NextPage } from "next";
+
+interface Props extends React.HTMLProps<HTMLDivElement> {
+  onClick: () => void;
+  isActive: boolean;
+}
+
+const Burger: NextPage<Props> = (props: Props) => (
+  <a className={"navbar-burger burger" + (props.isActive ? ' is-active' : '')} onClick={props.onClick}>
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+    <span aria-hidden="true"></span>
+  </a>
+);
+
+
+export default Burger;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,61 +1,73 @@
 import React from 'react';
-import { NextPage } from "next";
+import {NextPage} from "next";
 import NavbarLink from "./NavbarLink/NavbarLink";
 import NavbarDropdown from "./NavbarDropdown/NavbarDropdown";
+import Burger from "./Burger/Burger";
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
   language: 'cs' | 'pl';
 }
 
-const Header: NextPage<Props> = (props: Props) => (
-  <>
-  <nav className="navbar is-white">
-    <div className="navbar-brand">
-      <a className="" href="/">
-        <img src="/images/logo1.svg" alt="FairTranslations | Mgr. Jolanta Tarabová" width="331" height="85" />
-      </a>
-    </div>
+const Header: NextPage<Props> = (props: Props) => {
+  const [isBurgerActive, setBurgerActive] = React.useState<boolean>(false);
 
-    <div className="navbar-menu">
-      <div className="navbar-end">
-        {props.language === 'cs' ? (
-          <>
-            <NavbarLink href="/cs/o-firme">O firmě</NavbarLink>
-            <NavbarDropdown title="Překlady a tlumočení">
-              <NavbarLink href="/cs/preklady">Překlady</NavbarLink>
-              <NavbarLink href="/cs/tlumoceni">Tlumočení</NavbarLink>
-            </NavbarDropdown>
-            <NavbarDropdown title="Jiné služby">
-              <NavbarLink href="/cs/zprostredkovani-sluzby-telefonovani-do-polska">Zprostředkování služby telefonování do Polska</NavbarLink>
-              <NavbarLink href="/cs/podpora-podnikatelum">Podpora podnikatelům</NavbarLink>
-              <NavbarLink href="/cs/zalozeni-firmy-v-polsku">Založení firmy v Polsku</NavbarLink>
-            </NavbarDropdown>
-            <NavbarLink href="/cs/podminky-a-ceny">Podmínky a ceny</NavbarLink>
-            <NavbarLink href="/cs/kontakt">Kontakt</NavbarLink>
-          </>
-          ) : (
-          <>
-            <NavbarLink href="/pl/o-firmie">O firmie</NavbarLink>
-            <NavbarDropdown title="Tłumaczenia ustne i pisemne">
-              <NavbarLink href="/pl/tlumaczenia-pisemne">Tłumaczenia pisemne</NavbarLink>
-              <NavbarLink href="/pl/tlumaczenia-ustne">Tłumaczenia ustne</NavbarLink>
-            </NavbarDropdown>
-            <NavbarDropdown title="Inne usługi">
-              <NavbarLink href="/pl/posrednictwo-w-rozmowach-telefonicznych">Pośrednictwo w rozmowach telefonicznych</NavbarLink>
-              <NavbarLink href="/pl/wsparcie-dla-osob-prowadzacych-dzialalnosc">Wsparcie dla osób prowadzących działalność</NavbarLink>
-            </NavbarDropdown>
-            <NavbarLink href="/pl/warunki-i-ceny">Warunki i ceny</NavbarLink>
-            <NavbarLink href="/pl/kontakt">Kontakt</NavbarLink>
-          </>
-          )}
+  const onBurgerPressHandler = () => setBurgerActive(!isBurgerActive);
 
-        <NavbarLink href="/cs"><img src="/images/flag-cz.svg" width="28" height="21" /></NavbarLink>
-        <NavbarLink href="/pl"><img src="/images/flag-pl.svg" width="28" height="21" /></NavbarLink>
-      </div>
-    </div>
-  </nav>
-  </>
-);
+  return (
+    <>
+      <nav className="navbar is-white">
+        <div className="navbar-brand">
+          <a className="" href="/">
+            <img src="/images/logo1.svg" alt="FairTranslations | Mgr. Jolanta Tarabová" width="331" height="85"/>
+          </a>
+
+          <Burger onClick={onBurgerPressHandler} isActive={isBurgerActive} />
+        </div>
+
+        <div className={"navbar-menu" + (isBurgerActive ? ' is-active' : '')}>
+          <div className="navbar-end">
+            {props.language === 'cs' ? (
+              <>
+                <NavbarLink href="/cs/o-firme">O firmě</NavbarLink>
+                <NavbarDropdown title="Překlady a tlumočení">
+                  <NavbarLink href="/cs/preklady">Překlady</NavbarLink>
+                  <NavbarLink href="/cs/tlumoceni">Tlumočení</NavbarLink>
+                </NavbarDropdown>
+                <NavbarDropdown title="Jiné služby">
+                  <NavbarLink href="/cs/zprostredkovani-sluzby-telefonovani-do-polska">Zprostředkování služby
+                    telefonování do Polska</NavbarLink>
+                  <NavbarLink href="/cs/podpora-podnikatelum">Podpora podnikatelům</NavbarLink>
+                  <NavbarLink href="/cs/zalozeni-firmy-v-polsku">Založení firmy v Polsku</NavbarLink>
+                </NavbarDropdown>
+                <NavbarLink href="/cs/podminky-a-ceny">Podmínky a ceny</NavbarLink>
+                <NavbarLink href="/cs/kontakt">Kontakt</NavbarLink>
+              </>
+            ) : (
+              <>
+                <NavbarLink href="/pl/o-firmie">O firmie</NavbarLink>
+                <NavbarDropdown title="Tłumaczenia ustne i pisemne">
+                  <NavbarLink href="/pl/tlumaczenia-pisemne">Tłumaczenia pisemne</NavbarLink>
+                  <NavbarLink href="/pl/tlumaczenia-ustne">Tłumaczenia ustne</NavbarLink>
+                </NavbarDropdown>
+                <NavbarDropdown title="Inne usługi">
+                  <NavbarLink href="/pl/posrednictwo-w-rozmowach-telefonicznych">Pośrednictwo w rozmowach
+                    telefonicznych</NavbarLink>
+                  <NavbarLink href="/pl/wsparcie-dla-osob-prowadzacych-dzialalnosc">Wsparcie dla osób prowadzących
+                    działalność</NavbarLink>
+                </NavbarDropdown>
+                <NavbarLink href="/pl/warunki-i-ceny">Warunki i ceny</NavbarLink>
+                <NavbarLink href="/pl/kontakt">Kontakt</NavbarLink>
+              </>
+            )}
+
+            <NavbarLink href="/cs"><img src="/images/flag-cz.svg" width="28" height="21"/></NavbarLink>
+            <NavbarLink href="/pl"><img src="/images/flag-pl.svg" width="28" height="21"/></NavbarLink>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
 
 
 export default Header;
